83b868581bdd1df2f300666290c600a7779ccb75
Denis Antonov <dnantonov@stud.etu.ru>
2020-09-22 12:46:17 +0300

    Fix tests

--- test_product_page.py
+++ test_product_page.py
@@ -23,12 +23,20 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
     
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
         page = ProductPage(browser, link)
         page.open()
         page.add_product_to_cart()
 
+
+@pytest.mark.need_review
+def test_guest_can_add_product_to_cart(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_cart()
 
 @pytest.mark.skip
 def test_message_disappeared_after_adding_product_to_basket(browser):
@@ -38,7 +46,6 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.find_cart_button_and_click()
     page.should_dissapear_of_success_message()
 
-
 @pytest.mark.skip
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -46,14 +53,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
-@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BasketPage(browser, link)
